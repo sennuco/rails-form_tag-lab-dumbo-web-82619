5887aa4705b28fecd01b422bf9b0590f5bb7b9ab
Theresa Cyrus <cyrustheresa@gmail.com>
2019-10-30 13:16:01 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,10 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new 
+    @student = Student.new
+  end 
 
   def new
     @student = Student.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   resources :students, only: [:index , :new]
   get '/students/:id', to: 'students#show', as: 'student'
+=======
+  resources :students
+
+>>>>>>> 4c3ef9942241a0b3dc21d1a885e594a407b4cac3
 end
